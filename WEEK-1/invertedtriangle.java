5ba53085a8bd29034a5cd5c16d48d1189d1041c4
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-20 22:12:48 +0530

    java: inverted triangle

---
+++ WEEK-1/invertedtriangle.java
@@ -0,0 +1,9 @@
+public class invertedtriangle {
+    public static void main(){
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
