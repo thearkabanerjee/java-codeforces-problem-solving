0d419ad6fe6b15c48e656b2ca988c40e54f006db
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-29 02:50:41 +0530

    java: print char

---
+++ WEEK_2/printchar.java
@@ -0,0 +1,11 @@
+package WEEK_2;
+
+public class printchar {
+    public static void main(String[] args){
+        
+        for (char a ='A'; a <= 'Z'; a++){
+            System.out.print(a+ " ");
+        }
+        System.out.println();
+    }
+}
