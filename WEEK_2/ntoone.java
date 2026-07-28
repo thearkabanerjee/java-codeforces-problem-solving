7ac9d8aa29784b0c7990770b7a0ef7846bffa7a0
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-28 16:14:55 +0530

    java : n to one

--- WEEK_2/ntoone.java
+++ WEEK_2/ntoone.java
@@ -1,5 +1,15 @@
 package WEEK_2;
 
+import java.util.*;
 public class ntoone {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+
+        for (int i = a; i>= 1; i--){
+            System.out.println(i);
+        }
+
+        sc.close();
+    }
 }
