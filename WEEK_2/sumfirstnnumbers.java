c588b4c88e9f93825b05f40bafca2800a37ead6b
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-29 11:16:34 +0530

    java: sum of first n numbers

--- WEEK_2/sumfirstnnumbers.java
+++ WEEK_2/sumfirstnnumbers.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class sumfirstnnumbers {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int sum = (a*(a+1))/2;
+        long a = sc.nextInt();
+        long sum = (a*(a+1))/2;
         System.out.println(sum);
         sc.close();
     }
