b7afc6c6b4dced3e08434d42e62e1edf6515fb9d
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-29 09:51:35 +0530

    java : tableofn

---
+++ WEEK_2/tableofn.java
@@ -0,0 +1,12 @@
+package WEEK_2;
+import java.util.Scanner;
+public class tableofn {
+    public static void main(String [] args){
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        for (int i = 1; i <= 10; i++){
+            System.out.println(a + " * " + i +" = "+ (a*i));
+        }
+        sc.close();
+    }
+}
