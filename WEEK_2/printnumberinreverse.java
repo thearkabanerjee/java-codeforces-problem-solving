0c63b0ddb02866601039aa3b45b5c1acdc8e6649
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-30 14:24:56 +0530

    java: printing nums in reverse

--- WEEK_2/printnumberinreverse.java
+++ WEEK_2/printnumberinreverse.java
@@ -6,13 +6,15 @@ public class printnumberinreverse {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         long a = sc.nextLong();
+        long b = 0 ;
 
         while (a > 0){
-            System.out.print(a % 10);
+            b *= 10;
+            b += (a % 10);
             a /= 10;
         }
 
-        System.out.println();
+        System.out.println(b);
         sc.close();
 
 
