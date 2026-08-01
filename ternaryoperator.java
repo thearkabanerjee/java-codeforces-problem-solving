86e63be3e3ef8bb34b99203f80d58796bcd2e1cb
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-01 19:09:39 +0530

    java : ternary operator

---
+++ ternaryoperator.java
@@ -0,0 +1,9 @@
+public class ternaryoperator {
+    public static void main(String[] args){
+
+        int score = 70;
+        String passOrFail = (score>=60)? "Pass": "Fail";
+        
+        System.out.println(passOrFail);
+    }
+}
