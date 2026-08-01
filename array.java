10153aff365663803e66caa1aef418df772b52a4
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-02 02:17:37 +0530

    getting to learn java arrays, ik a bit late with the exam just tomorrow

---
+++ array.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class array {
+    public static void main(String[] args){
+        String[] hello = {"hello1", "hello2", "hello3"};
+
+        System.out.println(Arrays.toString(hello));
+    }
+}
