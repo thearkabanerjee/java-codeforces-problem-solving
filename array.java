02b362c8d18871c89db56ad8c194d315a20b0621
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-02 02:27:51 +0530

    looping over strings

--- array.java
+++ array.java
@@ -4,11 +4,16 @@ public class array {
     public static void main(String[] args){
         String[] hello = {"hello1", "hello2", "hello3"};
         int[] wow = {1, 2, 3};
-        // String a = "arka";
+        String a = "arka";
 
-        System.out.println(Arrays.toString(wow));
-        System.out.println(wow[1]);
-        System.out.println(hello[0]);
-        System.out.println(hello[2]);
+        // System.out.println(Arrays.toString(wow));
+        // System.out.println(wow[1]);
+        // System.out.println(hello[0]);
+        // System.out.println(hello[2]);
+
+
+        for (int i = 0; i< a.length(); i++){
+            System.out.println(a.charAt(i));
+        }
     }
 }
