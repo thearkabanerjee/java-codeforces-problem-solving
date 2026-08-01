6d8de75c61dcead254886c226f89562e089bb338
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-01 19:12:58 +0530

    java: ternary operator

--- ternaryoperator.java
+++ ternaryoperator.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class ternaryoperator {
     public static void main(String[] args){
-
-        int score = 70;
+        Scanner sc = new Scanner (System.in);
+        int score = sc.nextInt();
         String passOrFail = (score>=60)? "Pass": "Fail";
+        String oddOrEven = (score % 2 ==0)? "Even" : "Odd";
         
         System.out.println(passOrFail);
+        System.out.println(oddOrEven);
+        sc.close();
     }
 }
