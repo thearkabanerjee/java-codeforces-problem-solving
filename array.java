898a0e2054d017393e78f99439d69961f2275f27
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-02 02:21:48 +0530

    java : arrays

--- array.java
+++ array.java
@@ -3,9 +3,11 @@ import java.util.*;
 public class array {
     public static void main(String[] args){
         String[] hello = {"hello1", "hello2", "hello3"};
+        int[] wow = {1, 2, 3};
+        // String a = "arka";
 
-        System.out.println(Arrays.toString(hello));
-        System.out.println(hello[1]);
+        System.out.println(Arrays.toString(wow));
+        System.out.println(wow[1]);
         System.out.println(hello[0]);
         System.out.println(hello[2]);
     }
