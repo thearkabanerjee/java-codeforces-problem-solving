33a88cd9437ccdf7c956b61d5ea9a31281cf23a7
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-01 18:53:28 +0530

    java: fstrings in java

---
+++ fstrings.java
@@ -0,0 +1,13 @@
+
+public class fstrings {
+    public static void main(String[] args){
+        int a = 12;
+        String hello = "wow";
+        String name = "Arka";
+
+        System.out.printf("hello you must be %s and you are of age %d", hello, a);
+        System.out.println();
+        System.out.printf("hello my name is %s and i think your age is %d and the word is %s", name, a, hello);
+        System.out.println();
+    }    
+}
