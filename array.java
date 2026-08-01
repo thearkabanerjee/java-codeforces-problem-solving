52c6300c9b950217ed24b771542c47077ea55c11
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-02 02:19:48 +0530

    java: learning arrays

--- array.java
+++ array.java
@@ -5,5 +5,8 @@ public class array {
         String[] hello = {"hello1", "hello2", "hello3"};
 
         System.out.println(Arrays.toString(hello));
+        System.out.println(hello[1]);
+        System.out.println(hello[0]);
+        System.out.println(hello[2]);
     }
 }
