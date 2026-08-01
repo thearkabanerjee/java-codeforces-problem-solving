66ef4470a0f5f86b4c798c00c8387995d9f1512a
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-01 19:21:52 +0530

    java: methods

---
+++ methods.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class methods {
+    public static void main(String[] args){
+        //methods
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+
+        happysong(a);
+        sc.close();
+        
+    }
+
+    public static void happysong(int a){
+        for (int i = 0; i < a; i++){
+            System.out.println("happy birthday to you");
+            System.out.println("happy birthday to you");
+            System.out.println("happy birthday dear user");
+            System.out.println("happy birthday to you\n");
+        }
+    }
+}
