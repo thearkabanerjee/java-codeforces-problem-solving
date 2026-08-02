eae29241de455932537a54b7a9244996d4348e7f
Arka Banerjee <arkabanerjee381@gmail.com>
2026-08-02 08:42:04 +0530

    java & python

---
+++ function.java
@@ -0,0 +1,15 @@
+public class function {
+    public static void main(String[] args){
+        int a = 4;
+        int b = 3;
+
+        System.out.println(add(a, b));
+    }
+
+    static double add(int a, int b){
+        return a+b;
+    }
+    static double add(int a, int b , int c){
+        return a+b+c;
+    }
+}
---
+++ function.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return (a+b)
+
+def add(a, b, c):
+    return (a+b+c)
+
+m = add(1, 2, 3)
+print (m)
